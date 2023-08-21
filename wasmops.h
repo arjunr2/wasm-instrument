b0e0808752bd564b9e3b0a2c78068367e5427a82
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-08-21 16:52:15 -0400

    Added wasm simd extension definitions

--- wasmops.h
+++ wasmops.h
@@ -1,7 +1,7 @@
 #pragma once
 /* Must be a C-compatible header for designated init */
 
-/* Opcode constants: 172 */
+/** Opcode constants: 172 **/
 #define WASM_OP_UNREACHABLE		0x00 /* "unreachable" */
 #define WASM_OP_NOP			0x01 /* "nop" */
 #define WASM_OP_BLOCK			0x02 /* "block" BLOCKT */
@@ -204,19 +204,313 @@
 
 
 
-/* Multibyte opcode classes */
+/** Multibyte opcode classes **/
 #define WASM_EXT1_GCREF 0xFB
 #define WASM_EXT1_FC 0xFC
 #define WASM_EXT1_SIMD 0xFD
 #define WASM_EXT1_THREADS  0xFE
 
-/* 0xFC extensions: Partially implemented */
+/** 0xFC extensions: Partially implemented **/
 #define WASM_OP_MEMORY_INIT 0xFC08 /* "memory.init", ImmSigs.DATA_MEMORY */
 #define WASM_OP_DATA_DROP 0xFC09 /* "data.drop", ImmSigs.DATA */
 #define WASM_OP_MEMORY_COPY 0xFC0A /* "memory.copy", ImmSigs.MEMORYCP */
 #define WASM_OP_MEMORY_FILL 0xFC0B /* "memory.fill", ImmSigs.MEMORY */
 
-/* Threads/Atomics: 0xFE extensions */
+/** SIMD: 0xFD extensions **/
+#define WASM_OP_V128_LOAD 0xFD00 /* "v128.load", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD8x8_S 0xFD01 /* "v128.load8x8_s", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD8x8_U 0xFD02 /* "v128.load8x8_u", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD16x4_S 0xFD03 /* "v128.load16x4_s", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD16x4_U 0xFD04 /* "v128.load16x4_u", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD32x2_S 0xFD05 /* "v128.load32x2_s", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD32x2_U 0xFD06 /* "v128.load32x2_u", ImmSigs.MEMARG */
+
+#define WASM_OP_V128_LOAD8_SPLAT 0xFD07 /* "v128.load8_splat", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD16_SPLAT 0xFD08 /* "v128.load16_splat", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD32_SPLAT 0xFD09 /* "v128.load32_splat", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD64_SPLAT 0xFD0A /* "v128.load64_splat", ImmSigs.MEMARG */
+
+#define WASM_OP_V128_STORE 0xFD0B /* "v128.store", ImmSigs.MEMARG */
+#define WASM_OP_V128_CONST 0xFD0C /* "v128.store", ImmSigs.V128 */
+
+#define WASM_OP_I8x16_SHUFFLE 0xFD0D /* "i8x16.shuffle", ImmSigs.LANEIDX */
+#define WASM_OP_I8x16_SWIZZLE 0xFD0E /* "i8x16.swizzle", ImmSigs.NONE */
+
+#define WASM_OP_I8x16_SPLAT 0xFD0F /* "i8x16.splat", ImmSigs.NONE */
+#define WASM_OP_I16x8_SPLAT 0xFD10 /* "i16x8.splat", ImmSigs.NONE */
+#define WASM_OP_I32x4_SPLAT 0xFD11 /* "i32x4.splat", ImmSigs.NONE */
+#define WASM_OP_I64x2_SPLAT 0xFD12 /* "i64x2.splat", ImmSigs.NONE */
+#define WASM_OP_F32x4_SPLAT 0xFD13 /* "f32x4.splat", ImmSigs.NONE */
+#define WASM_OP_F64x2_SPLAT 0xFD14 /* "f64x2.splat", ImmSigs.NONE */
+
+#define WASM_OP_I8x16_EXTRACT_LANE_S 0xFD15 /* "i8x16.extract_lane_s", ImmSigs.LANEIDX */
+#define WASM_OP_I8x16_EXTRACT_LANE_U 0xFD16 /* "i8x16.extract_lane_u", ImmSigs.LANEIDX */
+#define WASM_OP_I8x16_REPLACE_LANE 0xFD17 /* "i8x16.replace_lane", ImmSigs.LANEIDX */
+
+#define WASM_OP_I16x8_EXTRACT_LANE_S 0xFD18 /* "i16x8.extract_lane_s", ImmSigs.LANEIDX */
+#define WASM_OP_I16x8_EXTRACT_LANE_U 0xFD19 /* "i16x8.extract_lane_u", ImmSigs.LANEIDX */
+#define WASM_OP_I16x8_REPLACE_LANE 0xFD1A /* "i16x8.replace_lane", ImmSigs.LANEIDX */
+
+#define WASM_OP_I32x4_EXTRACT_LANE 0xFD1B /* "i32x4.extract_lane", ImmSigs.LANEIDX */
+#define WASM_OP_I32x4_REPLACE_LANE 0xFD1C /* "i32x4.replace_lane", ImmSigs.LANEIDX */
+#define WASM_OP_I64x2_EXTRACT_LANE 0xFD1D /* "i64x2.extract_lane", ImmSigs.LANEIDX */
+#define WASM_OP_I64x2_REPLACE_LANE 0xFD1E /* "i64x2.replace_lane", ImmSigs.LANEIDX */
+#define WASM_OP_F32x4_EXTRACT_LANE 0xFD1F /* "f32x4.extract_lane", ImmSigs.LANEIDX */
+#define WASM_OP_F32x4_REPLACE_LANE 0xFD20 /* "f32x4.replace_lane", ImmSigs.LANEIDX */
+#define WASM_OP_F64x2_EXTRACT_LANE 0xFD21 /* "f64x2.extract_lane", ImmSigs.LANEIDX */
+#define WASM_OP_F64x2_REPLACE_LANE 0xFD22 /* "f64x2.replace_lane", ImmSigs.LANEIDX */
+
+#define WASM_OP_I8x16_EQ 0xFD23 /* "i8x16.eq", ImmSigs.NONE */
+#define WASM_OP_I8x16_NE 0xFD24 /* "i8x16.ne", ImmSigs.NONE */
+#define WASM_OP_I8x16_LT_S 0xFD25 /* "i8x16.lt_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_LT_U 0xFD26 /* "i8x16.lt_u", ImmSigs.NONE */
+#define WASM_OP_I8x16_GT_S 0xFD27 /* "i8x16.gt_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_GT_U 0xFD28 /* "i8x16.gt_u", ImmSigs.NONE */
+#define WASM_OP_I8x16_LE_S 0xFD29 /* "i8x16.le_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_LE_U 0xFD2A /* "i8x16.le_u", ImmSigs.NONE */
+#define WASM_OP_I8x16_GE_S 0xFD2B /* "i8x16.ge_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_GE_U 0xFD2C /* "i8x16.ge_u", ImmSigs.NONE */
+
+#define WASM_OP_I16x8_EQ 0xFD2D /* "i16x8.eq", ImmSigs.NONE */
+#define WASM_OP_I16x8_NE 0xFD2E /* "i16x8.ne", ImmSigs.NONE */
+#define WASM_OP_I16x8_LT_S 0xFD2F /* "i16x8.lt_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_LT_U 0xFD30 /* "i16x8.lt_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_GT_S 0xFD31 /* "i16x8.gt_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_GT_U 0xFD32 /* "i16x8.gt_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_LE_S 0xFD33 /* "i16x8.le_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_LE_U 0xFD34 /* "i16x8.le_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_GE_S 0xFD35 /* "i16x8.ge_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_GE_U 0xFD36 /* "i16x8.ge_u", ImmSigs.NONE */
+
+#define WASM_OP_I32x4_EQ 0xFD37 /* "i32x4.eq", ImmSigs.NONE */
+#define WASM_OP_I32x4_NE 0xFD38 /* "i32x4.ne", ImmSigs.NONE */
+#define WASM_OP_I32x4_LT_S 0xFD39 /* "i32x4.lt_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_LT_U 0xFD3A /* "i32x4.lt_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_GT_S 0xFD3B /* "i32x4.gt_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_GT_U 0xFD3C /* "i32x4.gt_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_LE_S 0xFD3D /* "i32x4.le_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_LE_U 0xFD3E /* "i32x4.le_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_GE_S 0xFD3F /* "i32x4.ge_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_GE_U 0xFD40 /* "i32x4.ge_u", ImmSigs.NONE */
+
+#define WASM_OP_F32x4_EQ 0xFD41 /* "f32x4.eq", ImmSigs.NONE */
+#define WASM_OP_F32x4_NE 0xFD42 /* "f32x4.ne", ImmSigs.NONE */
+#define WASM_OP_F32x4_LT 0xFD43 /* "f32x4.lt", ImmSigs.NONE */
+#define WASM_OP_F32x4_GT 0xFD44 /* "f32x4.gt", ImmSigs.NONE */
+#define WASM_OP_F32x4_LE 0xFD45 /* "f32x4.le", ImmSigs.NONE */
+#define WASM_OP_F32x4_GE 0xFD46 /* "f32x4.ge", ImmSigs.NONE */
+
+#define WASM_OP_F64x2_EQ 0xFD47 /* "f64x2.eq", ImmSigs.NONE */
+#define WASM_OP_F64x2_NE 0xFD48 /* "f64x2.ne", ImmSigs.NONE */
+#define WASM_OP_F64x2_LT 0xFD49 /* "f64x2.lt", ImmSigs.NONE */
+#define WASM_OP_F64x2_GT 0xFD4A /* "f64x2.gt", ImmSigs.NONE */
+#define WASM_OP_F64x2_LE 0xFD4B /* "f64x2.le", ImmSigs.NONE */
+#define WASM_OP_F64x2_GE 0xFD4C /* "f64x2.ge", ImmSigs.NONE */
+
+#define WASM_OP_V128_NOT 0xFD4D /* "v128.not", ImmSigs.NONE */
+#define WASM_OP_V128_AND 0xFD4E /* "v128.and", ImmSigs.NONE */
+#define WASM_OP_V128_ANDNOT 0xFD4F /* "v128.andnot", ImmSigs.NONE */
+#define WASM_OP_V128_OR 0xFD50 /* "v128.or", ImmSigs.NONE */
+#define WASM_OP_V128_XOR 0xFD51 /* "v128.xor", ImmSigs.NONE */
+#define WASM_OP_V128_BITSELECT 0xFD52 /* "v128.bitselect", ImmSigs.NONE */
+#define WASM_OP_V128_ANY_TRUE 0xFD53 /* "v128.any_true", ImmSigs.NONE */
+
+#define WASM_OP_V128_LOAD8_LANE 0xFD54 /* "v128.load8_lane", ImmSigs.MEMARG_LANEIDX */
+#define WASM_OP_V128_LOAD16_LANE 0xFD55 /* "v128.load16_lane", ImmSigs.MEMARG_LANEIDX */
+#define WASM_OP_V128_LOAD32_LANE 0xFD56 /* "v128.load32_lane", ImmSigs.MEMARG_LANEIDX */
+#define WASM_OP_V128_LOAD64_LANE 0xFD57 /* "v128.load64_lane", ImmSigs.MEMARG_LANEIDX */
+#define WASM_OP_V128_STORE8_LANE 0xFD58 /* "v128.store8_lane", ImmSigs.MEMARG_LANEIDX */
+#define WASM_OP_V128_STORE16_LANE 0xFD59 /* "v128.store16_lane", ImmSigs.MEMARG_LANEIDX */
+#define WASM_OP_V128_STORE32_LANE 0xFD5A /* "v128.store32_lane", ImmSigs.MEMARG_LANEIDX */
+#define WASM_OP_V128_STORE64_LANE 0xFD5B /* "v128.store64_lane", ImmSigs.MEMARG_LANEIDX */
+
+#define WASM_OP_V128_LOAD32_ZERO 0xFD5C /* "v128.load32_zero", ImmSigs.MEMARG */
+#define WASM_OP_V128_LOAD64_ZERO 0xFD5D /* "v128.load64_zero", ImmSigs.MEMARG */
+
+#define WASM_OP_F32x4_DEMOTE_F64x2_ZERO 0xFD5E /* "f32x4.demote_f64x2_zero", ImmSigs.NONE */
+#define WASM_OP_F64x2_PROMOTE_F32x4 0xFD5F /* "f64x2.demote_f32x4", ImmSigs.NONE */
+
+#define WASM_OP_I8x16_ABS 0xFD60 /* "i8x16.abs", ImmSigs.NONE */
+#define WASM_OP_I8x16_NEG 0xFD61 /* "i8x16.neg", ImmSigs.NONE */
+#define WASM_OP_I8x16_POPCNT 0xFD62 /* "i8x16.popcnt", ImmSigs.NONE */
+#define WASM_OP_I8x16_ALL_TRUE 0xFD63 /* "i8x16.all_true", ImmSigs.NONE */
+#define WASM_OP_I8x16_BITMASK 0xFD64 /* "i8x16.bitmask", ImmSigs.NONE */
+#define WASM_OP_I8x16_NARROW_I16x8_S 0xFD65 /* "i8x16.narrow_i16x8_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_NARROW_I16x8_U 0xFD66 /* "i8x16.narrow_i16x8_u", ImmSigs.NONE */
+
+#define WASM_OP_F32x4_CEIL 0xFD67 /* "f32x4.ceil", ImmSigs.NONE */
+#define WASM_OP_F32x4_FLOOR 0xFD68 /* "f32x4.floor", ImmSigs.NONE */
+#define WASM_OP_F32x4_TRUNC 0xFD69 /* "f32x4.trunc", ImmSigs.NONE */
+#define WASM_OP_F32x4_NEAREST 0xFD6A /* "f32x4.nearest", ImmSigs.NONE */
+
+#define WASM_OP_I8x16_SHL 0xFD6B /* "i8x16.shl", ImmSigs.NONE */
+#define WASM_OP_I8x16_SHR_S 0xFD6C /* "i8x16.shr_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_SHR_U 0xFD6D /* "i8x16.shr_u", ImmSigs.NONE */
+#define WASM_OP_I8x16_ADD 0xFD6E /* "i8x16.add", ImmSigs.NONE */
+#define WASM_OP_I8x16_ADD_SAT_S 0xFD6F /* "i8x16.add_sat_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_ADD_SAT_U 0xFD70 /* "i8x16.add_sat_u", ImmSigs.NONE */
+#define WASM_OP_I8x16_SUB 0xFD71 /* "i8x16.sub", ImmSigs.NONE */
+#define WASM_OP_I8x16_SUB_SAT_S 0xFD72 /* "i8x16.sub_sat_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_SUB_SAT_U 0xFD73 /* "i8x16.sub_sat_u", ImmSigs.NONE */
+
+#define WASM_OP_F64x2_CEIL 0xFD74 /* "f64x2.ceil", ImmSigs.NONE */
+#define WASM_OP_F64x2_FLOOR 0xFD75 /* "f64x2.floor", ImmSigs.NONE */
+
+#define WASM_OP_I8x16_MIN_S 0xFD76 /* "i8x16.min_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_MIN_U 0xFD77 /* "i8x16.min_u", ImmSigs.NONE */
+#define WASM_OP_I8x16_MAX_S 0xFD78 /* "i8x16.max_s", ImmSigs.NONE */
+#define WASM_OP_I8x16_MAX_U 0xFD79 /* "i8x16.max_u", ImmSigs.NONE */
+
+#define WASM_OP_F64x2_TRUNC 0xFD7A /* "f64x2.trunc", ImmSigs.NONE */
+
+#define WASM_OP_I8x16_AVGR_U 0xFD7B /* "i8x16.avgr_u", ImmSigs.NONE */
+
+#define WASM_OP_I16x8_EXTADD_PAIRWISE_I8x16_S 0xFD7C /* "i16x8.extadd_pairwise_i8x16_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTADD_PAIRWISE_I8x16_U 0xFD7D /* "i16x8.extadd_pairwise_i8x16_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_EXTADD_PAIRWISE_I16x8_S 0xFD7E /* "i16x8.extadd_pairwise_i8x16_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_EXTADD_PAIRWISE_I16x8_U 0xFD7F /* "i16x8.extadd_pairwise_i8x16_u", ImmSigs.NONE */
+
+#define WASM_OP_I16x8_ABS 0xFD8001 /* "i16x8.abs", ImmSigs.NONE */
+#define WASM_OP_I16x8_NEG 0xFD8101 /* "i16x8.neg", ImmSigs.NONE */
+#define WASM_OP_I16x8_Q15MULR_SAT_S 0xFD8201 /* "i16x8.q15mulr_sat_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_ALL_TRUE 0xFD8301 /* "i16x8.all_true", ImmSigs.NONE */
+#define WASM_OP_I16x8_BITMASK 0xFD8401 /* "i16x8.bitmask", ImmSigs.NONE */
+#define WASM_OP_I16x8_NARROW_I32x4_S 0xFD8501 /* "i16x8.narrow_i32x4_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_NARROW_I32x4_U 0xFD8601 /* "i16x8.narrow_i32x4_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTEND_LOW_I8x16_S 0xFD8701 /* "i16x8.extend_low_i8x16_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTEND_HIGH_I8x16_S 0xFD8801 /* "i16x8.extend_high_i8x16_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTEND_LOW_I8x16_U 0xFD8901 /* "i16x8.extend_low_i8x16_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTEND_HIGH_I8x16_U 0xFD8A01 /* "i16x8.extend_high_i8x16_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_SHL 0xFD8B01 /* "i16x8.shl", ImmSigs.NONE */
+#define WASM_OP_I16x8_SHR_S 0xFD8C01 /* "i16x8.shr_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_SHR_U 0xFD8D01 /* "i16x8.shr_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_ADD 0xFD8E01 /* "i16x8.add", ImmSigs.NONE */
+#define WASM_OP_I16x8_ADD_SAT_S 0xFD8F01 /* "i16x8.add_sat_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_ADD_SAT_U 0xFD9001 /* "i16x8.add_sat_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_SUB 0xFD9101 /* "i16x8.sub", ImmSigs.NONE */
+#define WASM_OP_I16x8_SUB_SAT_S 0xFD9201 /* "i16x8.sub_sat_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_SUB_SAT_U 0xFD9301 /* "i16x8.sub_sat_u", ImmSigs.NONE */
+#define WASM_OP_F64x2_NEAREST 0xFD9401 /* "f64x2.nearest", ImmSigs.NONE */
+#define WASM_OP_I16x8_MUL 0xFD9501 /* "i16x8.mul", ImmSigs.NONE */
+#define WASM_OP_I16x8_MIN_S 0xFD9601 /* "i16x8.min_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_MIN_U 0xFD9701 /* "i16x8.min_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_MAX_S 0xFD9801 /* "i16x8.max_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_MAX_U 0xFD9901 /* "i16x8.max_u", ImmSigs.NONE */
+/* placeholder 9A */
+#define WASM_OP_I16x8_AVGR_U 0xFD9B01 /* "i16x8.max_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTMUL_LOW_I8x16_S 0xFD9C01 /* "i16x8.extmul_low_i8x16_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTMUL_HIGH_I8x16_S 0xFD9D01 /* "i16x8.extmul_high_i8x16_s", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTMUL_LOW_I8x16_U 0xFD9E01 /* "i16x8.extmul_low_i8x16_u", ImmSigs.NONE */
+#define WASM_OP_I16x8_EXTMUL_HIGH_I8x16_U 0xFD9F01 /* "i16x8.extmul_high_i8x16_u", ImmSigs.NONE */
+
+
+#define WASM_OP_I32x4_ABS 0xFDA001 /* "i32x4.abs", ImmSigs.NONE */
+#define WASM_OP_I32x4_NEG 0xFDA101 /* "i32x4.neg", ImmSigs.NONE */
+/* placeholder A2 */
+#define WASM_OP_I32x4_ALL_TRUE 0xFDA301 /* "i32x4.all_true", ImmSigs.NONE */
+#define WASM_OP_I32x4_BITMASK 0xFDA401 /* "i32x4.bitmask", ImmSigs.NONE */
+/* placeholder A5 */
+/* placeholder A6 */
+#define WASM_OP_I32x4_EXTEND_LOW_I16x8_S 0xFDA701 /* "i32x4.extend_low_i16x8_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_EXTEND_HIGH_I16x8_S 0xFDA801 /* "i32x4.extend_high_i16x8_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_EXTEND_LOW_I16x8_U 0xFDA901 /* "i32x4.extend_low_i16x8_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_EXTEND_HIGH_I16x8_U 0xFDAA01 /* "i32x4.extend_high_i16x8_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_SHL 0xFDAB01 /* "i32x4.shl", ImmSigs.NONE */
+#define WASM_OP_I32x4_SHR_S 0xFDAC01 /* "i32x4.shr_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_SHR_U 0xFDAD01 /* "i32x4.shr_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_ADD 0xFDAE01 /* "i32x4.add", ImmSigs.NONE */
+/* placeholder AF */
+/* placeholder B0 */
+#define WASM_OP_I32x4_SUB 0xFDB101 /* "i32x4.sub", ImmSigs.NONE */
+/* placeholder B2 */
+/* placeholder B3 */
+/* placeholder B4 */
+#define WASM_OP_I32x4_MUL 0xFDB501 /* "i32x4.mul", ImmSigs.NONE */
+#define WASM_OP_I32x4_MIN_S 0xFDB601 /* "i32x4.min_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_MIN_U 0xFDB701 /* "i32x4.min_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_MAX_S 0xFDB801 /* "i32x4.max_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_MAX_U 0xFDB901 /* "i32x4.max_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_DOT_I16x8_S 0xFDBA01 /* "i32x4.dot_i16x8_s", ImmSigs.NONE */
+/* placeholder BB */
+#define WASM_OP_I32x4_EXTMUL_LOW_I16x8_S 0xFDBC01 /* "i32x4.extmul_low_i16x8_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_EXTMUL_HIGH_I16x8_S 0xFDBD01 /* "i32x4.extmul_high_i16x8_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_EXTMUL_LOW_I16x8_U 0xFDBE01 /* "i32x4.extmul_low_i16x8_u", ImmSigs.NONE */
+#define WASM_OP_I32x4_EXTMUL_HIGH_I16x8_U 0xFDBF01 /* "i32x4.extmul_high_i16x8_u", ImmSigs.NONE */
+
+
+#define WASM_OP_I64x2_ABS 0xFDC001 /* "i64x2.abs", ImmSigs.NONE */
+#define WASM_OP_I64x2_NEG 0xFDC101 /* "i64x2.neg", ImmSigs.NONE */
+/* placeholder C2 */
+#define WASM_OP_I64x2_ALL_TRUE 0xFDC301 /* "i64x2.all_true", ImmSigs.NONE */
+#define WASM_OP_I64x2_BITMASK 0xFDC401 /* "i64x2.bitmask", ImmSigs.NONE */
+/* placeholder C5 */
+/* placeholder C6 */
+#define WASM_OP_I64x2_EXTEND_LOW_I32x4_S 0xFDC701 /* "i64x2.extend_low_i32x4_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_EXTEND_HIGH_I32x4_S 0xFDC801 /* "i64x2.extend_high_i32x4_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_EXTEND_LOW_I32x4_U 0xFDC901 /* "i64x2.extend_low_i32x4_u", ImmSigs.NONE */
+#define WASM_OP_I64x2_EXTEND_HIGH_I32x4_U 0xFDCA01 /* "i64x2.extend_high_i32x4_u", ImmSigs.NONE */
+#define WASM_OP_I64x2_SHL 0xFDCB01 /* "i64x2.shl", ImmSigs.NONE */
+#define WASM_OP_I64x2_SHR_S 0xFDCC01 /* "i64x2.shr_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_SHR_U 0xFDCD01 /* "i64x2.shr_u", ImmSigs.NONE */
+#define WASM_OP_I64x2_ADD 0xFDCE01 /* "i64x2.add", ImmSigs.NONE */
+/* placeholder CF */
+/* placeholder D0 */
+#define WASM_OP_I64x2_SUB 0xFDD101 /* "i64x2.sub", ImmSigs.NONE */
+/* placeholder D2 */
+/* placeholder D3 */
+/* placeholder D4 */
+#define WASM_OP_I64x2_MUL 0xFDD501 /* "i64x2.mul", ImmSigs.NONE */
+#define WASM_OP_I64x2_EQ 0xFDD601 /* "i64x2.eq", ImmSigs.NONE */
+#define WASM_OP_I64x2_NE 0xFDD701 /* "i64x2.ne", ImmSigs.NONE */
+#define WASM_OP_I64x2_LT_S 0xFDD801 /* "i64x2.lt_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_GT_S 0xFDD901 /* "i64x2.gt_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_LE_S 0xFDDA01 /* "i64x2.le_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_GE_S 0xFDDB01 /* "i64x2.ge_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_EXTMUL_LOW_I32x4_S 0xFDDC01 /* "i64x2.extmul_low_i32x4_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_EXTMUL_HIGH_I32x4_S 0xFDDD01 /* "i64x2.extmul_high_i32x4_s", ImmSigs.NONE */
+#define WASM_OP_I64x2_EXTMUL_LOW_I32x4_U 0xFDDE01 /* "i64x2.extmul_low_i32x4_u", ImmSigs.NONE */
+#define WASM_OP_I64x2_EXTMUL_HIGH_I32x4_U 0xFDDF01 /* "i64x2.extmul_high_i32x4_u", ImmSigs.NONE */
+
+#define WASM_OP_F32x4_ABS 0xFDE001 /* "f32x4.abs", ImmSigs.NONE */
+#define WASM_OP_F32x4_NEG 0xFDE101 /* "f32x4.neg", ImmSigs.NONE */
+/* placeholder E2 */
+#define WASM_OP_F32x4_SQRT 0xFDE301 /* "f32x4.sqrt", ImmSigs.NONE */
+#define WASM_OP_F32x4_ADD 0xFDE401 /* "f32x4.add", ImmSigs.NONE */
+#define WASM_OP_F32x4_SUB 0xFDE501 /* "f32x4.sub", ImmSigs.NONE */
+#define WASM_OP_F32x4_MUL 0xFDE601 /* "f32x4.mul", ImmSigs.NONE */
+#define WASM_OP_F32x4_DIV 0xFDE701 /* "f32x4.div", ImmSigs.NONE */
+#define WASM_OP_F32x4_MIN 0xFDE801 /* "f32x4.min", ImmSigs.NONE */
+#define WASM_OP_F32x4_MAX 0xFDE901 /* "f32x4.max", ImmSigs.NONE */
+#define WASM_OP_F32x4_PMIN 0xFDEA01 /* "f32x4.pmin", ImmSigs.NONE */
+#define WASM_OP_F32x4_PMAX 0xFDEB01 /* "f32x4.pmax", ImmSigs.NONE */
+
+#define WASM_OP_F64x2_ABS 0xFDEC01 /* "f64x2.abs", ImmSigs.NONE */
+#define WASM_OP_F64x2_NEG 0xFDED01 /* "f64x2.neg", ImmSigs.NONE */
+/* placeholder EE */
+#define WASM_OP_F64x2_SQRT 0xFDEF01 /* "f64x2.sqrt", ImmSigs.NONE */
+#define WASM_OP_F64x2_ADD 0xFDF001 /* "f64x2.add", ImmSigs.NONE */
+#define WASM_OP_F64x2_SUB 0xFDF101 /* "f64x2.sub", ImmSigs.NONE */
+#define WASM_OP_F64x2_MUL 0xFDF201 /* "f64x2.mul", ImmSigs.NONE */
+#define WASM_OP_F64x2_DIV 0xFDF301 /* "f64x2.div", ImmSigs.NONE */
+#define WASM_OP_F64x2_MIN 0xFDF401 /* "f64x2.min", ImmSigs.NONE */
+#define WASM_OP_F64x2_MAX 0xFDF501 /* "f64x2.max", ImmSigs.NONE */
+#define WASM_OP_F64x2_PMIN 0xFDF601 /* "f64x2.pmin", ImmSigs.NONE */
+#define WASM_OP_F64x2_PMAX 0xFDF701 /* "f64x2.pmax", ImmSigs.NONE */
+
+#define WASM_OP_I32x4_TRUNC_SAT_F32x4_S 0xFDF801 /* "i32x4.trunc_sat_f32x4_s", ImmSigs.NONE */
+#define WASM_OP_I32x4_TRUNC_SAT_F32x4_U 0xFDF901 /* "i32x4.trunc_sat_f32x4_u", ImmSigs.NONE */
+#define WASM_OP_F32x4_TRUNC_SAT_I32x4_S 0xFDFA01 /* "f32x4.trunc_sat_i32x4_s", ImmSigs.NONE */
+#define WASM_OP_F32x4_TRUNC_SAT_I32x4_U 0xFDFB01 /* "f32x4.trunc_sat_i32x4_u", ImmSigs.NONE */
+
+#define WASM_OP_I32x4_TRUNC_SAT_F64x2_S_ZERO 0xFDFC01 /* "i32x4.trunc_sat_f64x2_s_zero", ImmSigs.NONE */
+#define WASM_OP_I32x4_TRUNC_SAT_F64x2_U_ZERO 0xFDFD01 /* "i32x4.trunc_sat_f64x2_u_zero", ImmSigs.NONE */
+#define WASM_OP_F64x2_CONVERT_LOW_I32x4_S 0xFDFE01 /* "f64x2.convert_low_i32x4_s", ImmSigs.NONE */
+#define WASM_OP_F64x2_CONVERT_LOW_I32x4_U 0xFDFF01 /* "f64x2.convert_low_i32x4_u", ImmSigs.NONE */
+
+
+
+
+/** Threads/Atomics: 0xFE extensions **/
 #define WASM_OP_MEMORY_ATOMIC_NOTIFY 0xFE00 /* "memory.atomic.notify", ImmSigs.MEMARG */
 #define WASM_OP_MEMORY_ATOMIC_WAIT32 0xFE01 /* "memory.atomic.wait32", ImmSigs.MEMARG */
 #define WASM_OP_MEMORY_ATOMIC_WAIT64 0xFE02 /* "memory.atomic.wait64", ImmSigs.MEMARG */
