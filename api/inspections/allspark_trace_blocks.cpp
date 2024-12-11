d964265734056c9fc4bfac1cc7ab1ceab74b15fb
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2024-12-11 10:15:11 -0500

    Add allspark trace inspection stub

---
+++ api/inspections/allspark_trace_blocks.cpp
@@ -0,0 +1,8 @@
+#include "inspect_common.h"
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+std::string allspark_trace_blocks_inspect (WasmModule &module) {
+    return std::string();
+}
